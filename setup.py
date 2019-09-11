35826c3a3fdf2d00b244a6b15da607259830193b
Caesar Kabalan <caesar.kabalan@gmail.com>
2019-09-11 05:59:18 +0000

    Add version computation logic

--- setup.py
+++ setup.py
@@ -1,11 +1,17 @@
+import os
 import setuptools
 
 with open("README.rst", "r") as fh:
     long_description = fh.read()
 
+version_from_tag = '0.0.1'
+if os.environ['CI_COMMIT_TAG']:
+    if os.environ['CI_COMMIT_TAG'].count('.') == 2:
+        version_from_tag = os.environ['CI_COMMIT_TAG']
+
 setuptools.setup(
     name="passthesecret-core",
-    version="0.1.0",
+    version=version_from_tag,
     author="Caesar Kabalan",
     author_email="caesar.kabalan@gmail.com",
     description="PassTheSecret core library containing back-end code for encryption and storage of secrets.",
